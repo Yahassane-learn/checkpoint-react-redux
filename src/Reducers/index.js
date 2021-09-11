928a0193c9cc6f462af66fce3218ccfea49b5fac
yahassane <84969138+Yahassane-learn@users.noreply.github.com>
2021-09-11 03:29:35 +0000

    Delete src directory

--- src/Reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import toDosReducer from './toDosReducers'
-import newToDosIdeas from './newToDosIdeas'
-
-export default combineReducers({toDosReducer, newToDosIdeas})
